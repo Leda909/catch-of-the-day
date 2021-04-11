5a3d2b83b06ee3f24a747d0e413af19b23c92c57
Leda909 <ko9zad@gmail.com>
2021-04-11 14:48:43 +0100

    available, disavailable

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -6,6 +6,7 @@ class Fish extends React.Component {
         // This is the form if You want to write out each variable separetly, however, there is a shorter, collective version above
         // const image = this.props.details.image;  
         const { image, name, price, desc, status } = this.props.details;
+        const isAvailable = status === "available";
 
         return ( 
         <li className="menu-fish">
@@ -15,7 +16,12 @@ class Fish extends React.Component {
                 <span className="price">{formatPrice(price)}</span>
             </h3>
             <p>{desc}</p>
-            <button>Add to cart</button>
+            <button
+                disabled={!isAvailable}
+                onClick={() => this.props.addToOrder(this.props.index)}
+                >
+                {isAvailable ? "Add To Order" : "Sold Out!"}
+            </button>
         </li>
         );
     }
