27ed35e5db6a72262da99ddafebaa7bc970ef01a
Leda909 <ko9zad@gmail.com>
2021-04-12 10:12:11 +0100

    persisting our state with firebase

---
+++ src/base.js
@@ -0,0 +1,15 @@
+import Rebase from "re-base";
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyBP2vgHWpUzU06f-J4pIVR5Ifx6-vHn4-Y",
+    authDomain: "my-fish-shopping-cart-project.firebaseapp.com",
+    databaseURL: "https://my-fish-shopping-cart-project-default-rtdb.europe-west1.firebasedatabase.app",
+});
+
+const base = Rebase.createClass(firebaseApp.database());
+
+//This is a named export
+export { firebaseApp };
+
+export default base;
--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Order from "./Order";
 import Inventory from "./Inventory";
 import sampleFishes from "../sample-fishes";
 import Fish from "./Fish";
+import base from "../base";
 
 class App extends React.Component {
   state = {
@@ -11,6 +12,19 @@ class App extends React.Component {
     order: {}
   };
   
+  componentDidMount() {
+    // console.log('mount');
+    const { params } = this.props.match;
+    this.ref = base.syncState(`${params.storeId}/fishes`,{
+      context : this,
+      state: 'fishes'
+    });
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+  }
+
   addFish = fish => {
       // console.log("Adding a Fish!");
     // 1. Take a copy of the existing state
