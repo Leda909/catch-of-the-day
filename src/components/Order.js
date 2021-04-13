7cfd37885a11fda66bfd8e048245656d2c1eddf9
Leda909 <ko9zad@gmail.com>
2021-04-13 18:58:51 +0100

    animating react components 02

--- src/components/Order.js
+++ src/components/Order.js
@@ -7,24 +7,37 @@ class Order extends React.Component {
     const fish = this.props.fishes[key];
     const count = this.props.order[key];
     const isAvailable = fish && fish.status === 'available';
+    const transitionOption = {
+      classNames: "order", 
+      key,
+      timeout:{ enter: 500, exit: 500 }
+    }
     // Make sure the fish is loaded before we continue!
     if(!fish) return null;
 
     if(!isAvailable) {
       return (
-        <CSSTransition classNames="order" key={key} timeout={{ enter: 250, exit: 250 }}>
+        <CSSTransition {...transitionOption}>
           <li key={key}>Sorry {fish ? fish.name : 'fish'} is no longer available</li>
         </CSSTransition>
       );
     }
+
     return (
-      <CSSTransition classNames="order" key={key} timeout={{ enter: 250, exit: 250 }}>
+      <CSSTransition {...transitionOption}>
         <li key={key}>
-          {count} lbs {fish.name}
-          {formatPrice(count * fish.price)}
-          <button onClick={() => this.props.removeFromOrder(key)}>
-                &times;
-          </button>
+          <span>
+            <TransitionGroup component="span" calssName="count">
+              <CSSTransition {...transitionOption}>
+                <span>{count}</span>
+              </CSSTransition>
+            </TransitionGroup>
+            lbs {fish.name}
+            {formatPrice(count * fish.price)}
+            <button onClick={() => this.props.removeFromOrder(key)}>
+              &times;
+            </button>
+          </span>
         </li>
       </CSSTransition>
     );
@@ -32,14 +45,14 @@ class Order extends React.Component {
 
   render() {
     const orderIds = Object.keys(this.props.order);
-    const total = orderIds.reduce((prevTotal, key) =>{
+    const total = orderIds.reduce((prevTotal, key) => {
       const fish = this.props.fishes[key];
       const count = this.props.order[key];
-      const isAvailable = fish && fish.status === 'available';
-        if(isAvailable) {
-          return prevTotal + (count * fish.price);
-        }
-        return prevTotal;
+      const isAvailable = fish && fish.status === "available";
+      if (isAvailable) {
+        return prevTotal + count * fish.price;
+      }
+      return prevTotal;
     }, 0);
     return (
       <div className="order-wrap">
